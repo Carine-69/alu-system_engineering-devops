a486309e9be719128cf170400102fb848b0fd361
Carine-69 <umugabecarine@gmail.com>
2024-07-09 15:07:29 +0000

    create ssh

---
+++ ssh/1-create_ssh_key_pair.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash 
+
+# Generate a 4096-bit RSA key pair and save it to /root/.ssh/school
+ssh-keygen -b 4096 -f /root/.ssh/school -t rsa -N betty
+
+# Ensure correct permissions for the private key
+chmod 600 /root/.ssh/school
+
