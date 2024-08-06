ec504aca0a7f24133a86e275328cb2d9c720e1a4
Carine-69 <umugabecarine@gmail.com>
2024-08-06 12:20:21 +0000

    debugging

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,21 @@
-sudo apt-get update
-sudo apt-get install apache2
+# This Puppet manifest configures Nginx to handle high load by optimizing settings
+
+exec { 'fix--for-nginx':
+  command => '/usr/sbin/nginx -s reload',
+}
+
+# Additional Nginx configurations for performance optimization can go here
+# For example, adjusting worker processes and connections
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('web_stack_debugging_4/nginx.conf.erb'),
+  notify  => Exec['fix--for-nginx'],
+}
+
+# Ensure the Nginx service is running
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
 
